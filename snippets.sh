fac0952374c5ff886f17793ae13d9f7b4543170c
Brennan Stehling <brennan@smallsharptools.com>
2013-03-14 11:12:53 -0500

    added script to make it easier to manage snippets

---
+++ snippets.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+GitRoot="`dirname \"$0\"`"
+
+case "$1" in
+    update)
+        echo Updating...
+        pushd .
+        cd ${GitRoot}
+        git pull
+        cp sst_*.codesnippet ~/Library/Developer/Xcode/UserData/CodeSnippets/
+        popd
+        ;;
+    find)
+        echo Finding...
+        ls ~/Library/Developer/Xcode/UserData/CodeSnippets/*.codesnippet | grep -v sst_
+        ;;
+    import)
+        echo Importing...
+        pushd .
+        cd ${GitRoot}
+        cp ~/Library/Developer/Xcode/UserData/CodeSnippets/sst_*.codesnippet .
+        popd
+        ;;
+    export)
+        echo Exporting...
+        cp ${GitRoot}/sst_*.codesnippet ~/Library/Developer/Xcode/UserData/CodeSnippets/
+        ;;
+    clear)
+        echo Clearing...
+        pushd .
+        cd ${GitRoot}
+        rm ~/Library/Developer/Xcode/UserData/CodeSnippets/sst_*.codesnippet
+        popd
+        ;;
+    *)
+        echo "Usage: `basename $0` { update | find | import | export | clear }"
+        ;;
+esac
+
